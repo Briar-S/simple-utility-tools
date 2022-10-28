2baf3afd8ff03d01f9a31c0a4ec64fa05121e52a
Briar Sanders <jbsanders@bastiansolutions.com>
2022-10-28 16:02:38 -0400

    fixed some silly bugs, should be perf now. i hate whitespace

--- Text Compare/text_file_compare_util.py
+++ Text Compare/text_file_compare_util.py
@@ -1,63 +1,82 @@
 import PySimpleGUI as sg
-sg.theme('Light Grey 6')
+import os
+class TextCompareUtility():
+    sg.theme('Light Grey 6')
 
-def CompareFiles(f1, f2, f3, swap, owrite):
-    curF = open(f1, "r")
-    curFile = curF.readlines()
-    curF.close()
-
-    oldF = open(f2, "r")
-    oldFile = oldF.readlines()
-    oldF.close()
-
-    if(owrite == True):
-        outF = open(f3, "w")
-    else:
-        outF = open(f3, "a")
-
-    if(swap == True):
-        outF.write("Following lines exist in " + f1 + " but not in " + f2)
-        curFile, oldFile = oldFile, curFile
-    else:
-        outF.write("Following lines exist in " + f2 + " but not in " + f1)
-
-    outF.write("\n=================\n")
-    for line in oldFile:
-        if (curFile.count(line) == 0):
-            outF.write(line)
-    outF.write("\n=================\n")
-    oldF.close()
-
-form_rows = [[sg.Text('Enter 2 files to comare')],
+    def TxtCompLayout():
+        #This creates the basic layout of the window using pysimpleGUI and returns a window value
+        form_rows = [[sg.Text('Enter 2 files to comare')],
              [sg.Text('Checks if file 2 contains lines that file 1 does not')],
              [sg.HorizontalSeparator()],
-             [sg.Text('File 1', size=(10, 1)),
-                sg.InputText(key='-file1-'), sg.FileBrowse()],
-             [sg.Text('File 2', size=(10, 1)), sg.InputText(key='-file2-'),
-              sg.FileBrowse(target='-file2-')],
-             [sg.Text('Output', size=(10, 1)), sg.InputText(key='-file3-'),
-              sg.FileBrowse(target='-file3-')],
+                [sg.Text('File 1', size=(10, 1)), sg.InputText(key='-file1-'), sg.FileBrowse()],
+                [sg.Text('File 2', size=(10, 1)), sg.InputText(key='-file2-'), sg.FileBrowse(target='-file2-')],
+                [sg.Text('Output', size=(10, 1)), sg.InputText(key='-file3-'), sg.FileBrowse(target='-file3-')],
              [sg.HorizontalSeparator()],
              [sg.Text('Options')],
-             [sg.Checkbox('Swap comparison direction?', default=False, key="-swap-")],
-             [sg.Radio('Append', "RADIO1", default=True)],
-             [sg.Radio('Overwrite', "RADIO1", default=False, key="-overwrite-")],
+                [sg.Checkbox('Swap comparison direction?', default=False, key="-swap-")],
+                [sg.Radio('Append', "RADIO1", default=True)],
+                [sg.Radio('Overwrite', "RADIO1", default=False, key="-overwrite-")],
+             [sg.HorizontalSeparator()],
              [sg.Submit(), sg.Cancel()]]
-             
-window = sg.Window('File Compare', form_rows, resizable=True, finalize=True)
 
-while True:
-    button, values = window.read()
+        return sg.Window('File Compare', form_rows, resizable=True, finalize=True)
 
-    f1, f2, f3 = values['-file1-'], values['-file2-'], values['-file3-']
+    def CompareFiles(f1, f2, f3, swap, owrite):
+        with open(f1,'a+') as fone:
+            fone.write('\n')
+        with open(f2,'a+') as ftwo:
+            ftwo.write('\n')
+        
+        curF = open(f1, "r")
+        curFile = curF.readlines()
+        curF.close()
 
-    if any((button == 'Cancel', button == "Exit", button == sg.WIN_CLOSED)):
-        break
+        oldF = open(f2, "r")
+        oldFile = oldF.readlines()
+        oldF.close()        
 
-    if any((button != 'Submit', f1 == '', f2 == '', f3 == '')):
-        sg.popup_error('Error, likely missing a file path in selection')
+        with open(f1, 'rb+') as filehandle:
+            filehandle.seek(-2, os.SEEK_END)
+            filehandle.truncate()
 
-    if(button == 'Submit' and f1 != '' and f2 != '' and f3 != ''):
-        CompareFiles(f1, f2, f3, values['-swap-'], values['-overwrite-'])
-        sg.popup('Files Compared Successfully')
-window.close()
+        with open(f2, 'rb+') as filehandle:
+            filehandle.seek(-2, os.SEEK_END)
+            filehandle.truncate()
+
+        if(owrite == True):
+            outF = open(f3, "w")
+        else:
+            outF = open(f3, "a")
+
+        outF.write("\n=================\n")
+        if(swap == True):
+            outF.write("Following lines exist in " + f1 + " but not in " + f2)
+            curFile, oldFile = oldFile, curFile
+        else:
+            outF.write("Following lines exist in " + f2 + " but not in " + f1)
+
+        outF.write("\n=================\n")
+        for line in oldFile:
+            if (curFile.count(line) == 0 and line != '' and line != None and line != ' ' and line !='\n'):
+                outF.write(line)
+
+        outF.close()
+    
+
+    while True:
+
+        button, values = TxtCompLayout.read() #Reads the window object values stored when interacting with the window
+
+        f1, f2, f3 = values['-file1-'], values['-file2-'], values['-file3-'] #stores file paths gotten from the window
+
+        if any((button == 'Cancel', button == "Exit", button == sg.WIN_CLOSED)): #if X, or cancel pressed, break loop (closes window)
+            break
+
+        if any((button != 'Submit', f1 == '', f2 == '', f3 == '')): #if a path isnt filled out, toss an error message (Dont close window)
+            sg.popup_error('Error, likely missing a file path in selection')
+
+        if(button == 'Submit' and f1 != '' and f2 != '' and f3 != ''):
+            CompareFiles(f1, f2, f3, values['-swap-'], values['-overwrite-'])
+            sg.popup('Files Compared Successfully')
+
+    window.close()
